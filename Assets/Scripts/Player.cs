033f5bb38deaf1f5adb6621fe8da9ac26f81f9c1
Iago Rosa <iagoimportantthings@gmail.com>
2025-10-23 20:45:33 -0300

    feat: player with animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public KeyCode moveRight = KeyCode.RightArrow;
+    public KeyCode moveLeft = KeyCode.LeftArrow;
+    public KeyCode jump = KeyCode.Space;
+    public float moveSpeed = 5f;
+    public float jumpForce = 20f;
+    public float boundX = 8.3f;
+    public float boundY = 3.9f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+    private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetBool("isMoving", false);
+
+        float horizontalDirection = 0.0f;
+
+        if (Input.GetKey(moveRight))
+        {
+            animator.SetBool("isMoving", true);
+            horizontalDirection = 1.0f;
+        }
+        else if(Input.GetKey(moveLeft))
+        {
+            animator.SetBool("isMoving", true);
+            horizontalDirection = -1.0f;
+        }
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        var hVelocity = rb.linearVelocity;
+        hVelocity.x = horizontalDirection * moveSpeed;
+        rb.linearVelocity = hVelocity;
+
+        var hPosition = transform.position;
+        hPosition.x = Mathf.Clamp(hPosition.x, -boundX, boundX);
+        transform.position = hPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Plataforma"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Plataforma"))
+        {
+            isGrounded = false;
+        }
+    }
+}
